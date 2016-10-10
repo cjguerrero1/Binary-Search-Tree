29e709e5e76109bdf9ebce55cbc6a4dd636a25b8
CJ Guerrero <CJ Guerrero>
2016-10-10 07:31:00 -0500

    initial commit

---
+++ BinarySearchTree/BinarySearchTree/MyBinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class MyBinaryTree
+    {
+        public Node root;
+        static int count;
+
+        public MyBinaryTree()
+        {
+            root = null;
+            count = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            if (root==null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Node AddNode(int value)
+        {
+            Node newNode = new Node(value);
+            if (root == null)
+            {
+                root = newNode;
+            }
+            count++;
+            return newNode;
+        }
+
+        public void Insert(Node root, Node newNode)
+        {
+            if(IsEmpty())
+            {
+                root = newNode;
+            }
+            else if(newNode.value < root.value)
+            {
+                if(root.left == null)
+                {
+                    root.left = newNode;
+                }
+                else
+                {
+                    root = root.left;
+                    Insert(root, newNode);
+                }
+            }
+            else if(newNode.value > root.value)
+            {
+                if (root.right == null)
+                {
+                    root.right = newNode;
+                }
+                else
+                {
+                    root = root.right;
+                    Insert(root, newNode);
+                }
+            }
+        }
+
+        public void DisplayTree(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                DisplayTree(root.left);
+                Console.Write("{0} ", root.value);
+                DisplayTree(root.right);
+            }
+        }       
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBinaryTree tree = new MyBinaryTree();
+            Node rootNode = tree.AddNode(10);
+
+            tree.Insert(tree.root, rootNode);
+            tree.Insert(tree.root, tree.AddNode(7));
+            tree.Insert(tree.root, tree.AddNode(12));
+            tree.Insert(tree.root, tree.AddNode(3));
+            tree.Insert(tree.root, tree.AddNode(16));
+            tree.Insert(tree.root, tree.AddNode(5));
+            tree.Insert(tree.root, tree.AddNode(14));
+            tree.DisplayTree(tree.root);
+            Console.ReadLine();
+        }
+    }
+}
